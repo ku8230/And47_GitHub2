97e3c5fcf9bedb3002dbb02e9f6507a280bb3890
ku8230 <ku8230@naver.com>
2018-10-19 14:43:50 +0900

    테스트

--- app/src/main/java/edu/android/and47_github2/MainActivity.java
+++ app/src/main/java/edu/android/and47_github2/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        textView.setText("테스트");
 
     }
 }
