9357471533c1f704e9154da6f0ab7faad897d158
ku8230 <ku8230@naver.com>
2018-10-19 14:42:04 +0900

    Initial commit

---
+++ app/src/main/java/edu/android/and47_github2/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.android.and47_github2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
